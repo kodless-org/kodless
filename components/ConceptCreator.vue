4dd7347f925e84c50b5a93818dbbf99ae55c1f39
Abutalib (Barish) Namazov <abutalib@mit.edu>
2024-03-28 12:50:36 -0400

    Support bulk creation

--- components/ConceptCreator.vue
+++ components/ConceptCreator.vue
@@ -9,31 +9,67 @@ const newConcept = reactive({
   name: "",
   prompt: "",
 });
+// TODO: use pending from useFetch later
 const loading = ref(false);
-const createConcept = async () => {
-  loading.value = true;
+const createConcept = async (name: string, prompt: string) => {
   await useFetch(`/api/projects/${project}/concepts/`, {
     method: "POST",
     body: {
-      concept: newConcept.name,
-      prompt: newConcept.prompt,
+      concept: name,
+      prompt: prompt,
     },
   });
+  emit("refresh");
+};
+
+const handleCreateConcept = async () => {
+  loading.value = true;
+  await createConcept(newConcept.name, newConcept.prompt);
   loading.value = false;
   newConcept.name = "";
   newConcept.prompt = "";
-  emit("refresh");
-};
+}
+
+const isBulk = ref(false);
+const bulkInput = ref("");
+
+const handleCreateBulk = async () => {
+  loading.value = true;
+  const splits = bulkInput.value.split("concept: ");
+
+  const concepts: { name: string; prompt: string }[] = [];
+
+  for (let i = 1; i < splits.length; i++) {
+    let [name, ...restPrompt] = splits[i].split(".");
+    const prompt = restPrompt.join(".");
+    name = name.replace(/[^a-zA-Z0-9]/g, "");
+    concepts.push({ name, prompt });
+  }
+
+  await Promise.all(concepts.map(({ name, prompt }) => createConcept(name, prompt)));
+
+  loading.value = false;
+  bulkInput.value = "";
+}
 </script>
 
 <template>
-  <n-form>
+  <label>
+    <input type="checkbox" v-model="isBulk" /> Bulk create
+  </label>
+  <n-form v-if="!isBulk">
     <n-form-item label="Concept name">
       <n-input v-model:value="newConcept.name" />
     </n-form-item>
     <n-form-item label="Description">
       <n-input v-model:value="newConcept.prompt" type="textarea" />
     </n-form-item>
-    <n-button @click="createConcept" :loading="loading" type="primary">Create</n-button>
+    <n-button @click="handleCreateConcept" :loading="loading" type="primary">Create</n-button>
+  </n-form>
+  <n-form v-else>
+    <n-form-item label="Enter descriptions, separated by 'concept: <ConceptName>.' lines">
+      <n-input v-model:value="bulkInput" type="textarea" />
+    </n-form-item>
+    <n-button @click="handleCreateBulk" :loading="loading" type="primary">Create</n-button>
   </n-form>
 </template>
--- components/RouteCreator.vue
+++ components/RouteCreator.vue
@@ -7,25 +7,61 @@ const emit = defineEmits(["refresh"]);
 
 const prompt = ref("");
 const pending = ref(false);
-const createRoute = async () => {
-  pending.value = true;
+// TODO: use pending from useFetch later
+const createRoute = async (prompt: string) => {
   await useFetch(`/api/projects/${project}/routes/`, {
     method: "POST",
     body: {
-      prompt: prompt.value,
+      prompt,
     },
   });
-  pending.value = false;
-  prompt.value = "";
   emit("refresh");
 };
+
+const handleCreateRoute = async () => {
+  pending.value = true;
+  await createRoute(prompt.value);
+  pending.value = false;
+  prompt.value = "";
+}
+
+const isBulk = ref(false);
+const bulkInput = ref("");
+
+const handleCreateBulk = async () => {
+  pending.value = true;
+  const splits = bulkInput.value.split("\n");
+
+  const routes: { prompt: string }[] = [];
+
+  for (let i = 0; i < splits.length; i++) {
+    let prompt = splits[i].trim();
+    if (!prompt) continue;
+    routes.push({ prompt });
+  }
+
+  await Promise.all(routes.map(({ prompt }) => createRoute(prompt)));
+
+  pending.value = false;
+  bulkInput.value = "";
+}
+
 </script>
 
 <template>
-  <n-form>
+  <label>
+    <input type="checkbox" v-model="isBulk" /> Bulk create
+  </label>
+  <n-form v-if="!isBulk">
     <n-form-item label="Route description">
       <n-input v-model:value="prompt" type="textarea" />
     </n-form-item>
-    <n-button @click="createRoute" :loading="pending">Create</n-button>
+    <n-button @click="handleCreateRoute" :loading="pending">Create</n-button>
+  </n-form>
+  <n-form v-else>
+    <n-form-item label="One route description per line.">
+      <n-input v-model:value="bulkInput" type="textarea" />
+    </n-form-item>
+    <n-button @click="handleCreateBulk" :loading="pending">Create</n-button>
   </n-form>
 </template>
